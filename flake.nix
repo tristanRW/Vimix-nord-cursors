1074233b67fbff0dd810d3b81eed1d3b27102b23
Rainhard <79083189+tristanRW@users.noreply.github.com>
2024-01-07 21:43:16 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   };
 
-  outputs = {nixpkgs}:
+  outputs = {self, nixpkgs}:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
